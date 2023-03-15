93ec63d1138cc4b92ae9414184c9a4dc8c54f6c9
Mukul <mukultotla4@gmail.com>
2023-03-15 18:19:43 +0530

    Graph QL API - Node JS

---
+++ FakeData.js
@@ -0,0 +1,32 @@
+const UserList = [
+    {
+        id: 1,
+        name: "Mukul", 
+        username: "mukultotla",
+        age: 25,
+        nationality: "India"
+    },
+    {
+        id: 2,
+        name: "Mukul", 
+        username: "mukultotla",
+        age: 19,
+        nationality: "India"
+    },
+    {
+        id: 3,
+        name: "Mukul", 
+        username: "mukultotla",
+        age: 32,
+        nationality: "India"
+    },
+    {
+        id: 4,
+        name: "Mukul", 
+        username: "mukultotla",
+        age: 58,
+        nationality: "India"
+    }
+];
+
+module.exports = {UserList}
---
+++ schema/resolvers.js
@@ -0,0 +1,9 @@
+const {UserList} = require('../FakeData')
+const resolvers = {
+    Query: {
+        users(){
+            return UserList
+        }
+    }
+}
+module.exports = {resolvers}
